ab3bf490bfd356d20d14bc602a30608e77c0802d
speedcuberayush <msdakm3334@gmail.com>
2024-07-14 21:12:10 +0530

    session main

--- 01_Arrays/1D ARRAY/12.cpp
+++ 01_Arrays/1D ARRAY/12.cpp
@@ -3,40 +3,34 @@ using namespace std;
 
 void print(const vector<int> &arr)
 {
+
     for (auto it : arr)
         cout << it << " ";
     cout << endl;
 }
 
-// approach 1
-int solve(vector<int> arr)
-{
-    int ans = 0;
-    int sum = accumulate(arr.begin(), arr.end(), 0);
-    int n = *max_element(arr.begin(), arr.end());
-    return (n * (n + 1) / 2) - sum;
-}
-// approach 2
-// int solve(vector<int> arr)
-// {
-//     sort(arr.begin(), arr.end());
-//     if (arr[0] != 1)
-//         return 1;
-//     for (int i = 0; i < arr.size(); i++)
-//     {
-//         if (arr[i] + 1 != arr[i + 1])
-//             return arr[i] + 1;
-//     }
-//     return -1;
-// }
-
 int main()
 {
-    vector<int> arr = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> arr = {1, 2, 1, 2, 3, 3, 4, 6, 7, 8, 7, 6, 8};
     int n = arr.size();
+
     cout << "ORIGINIAL: ";
     print(arr);
-    cout << solve(arr);
 
     return 0;
 }
+/*
+void sort(vector<int> &arr,int s,int e){
+if(s<e){
+int i=s-1;
+for(int j=0;j<e;j++)
+if(arr[j]<arr[e])
+swap(arr[++i],arr[j])
+swap(arr[++i],arr[e])
+sort(arr,s,i-1);
+sort(arr,i+1,e);
+}
+
+}
+
+*/
--- 01_Arrays/1D ARRAY/temp.cpp
+++ 01_Arrays/1D ARRAY/temp.cpp
@@ -1,66 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<int, int>> solve1(vector<int> arr, int n, int sum)
+
+void sort(vector<int> &arr, int s, int e)
 {
-    vector<pair<int, int>> ans;
-    sort(arr.begin(), arr.end());
-    int i = 0;
-    int j = n - 1;
-    vector<int> p;
-    while (i < j)
+    if (s < e)
     {
-        if (arr[i] + arr[j] == sum)
-            ans.push_back({arr[i++], arr[j++]});
-        else if (arr[i] + arr[j] > sum)
-            j--;
-        else
-            i++;
+
+        int i = s - 1;
+        for (int j = s; j < e; j++)
+            if (arr[j] < arr[e])
+                swap(arr[++i], arr[j]);
+        swap(arr[++i], arr[e]);
+        sort(arr, s, i - 1);
+        sort(arr, i + 1, e);
     }
-    return ans;
 }
-vector<pair<int, int>> solve2(vector<int> arr, int n, int sum)
+
+void print(vector<int> arr)
 {
-    vector<pair<int, int>> ans;
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (arr[i] + arr[j] == sum)
-                ans.push_back({arr[i], arr[j]});
 
-    return ans;
-}
-vector<pair<int, int>> solve3(vector<int> arr, int n, int sum)
-{
-    vector<pair<int, int>> ans;
-    unordered_map<int, int> mpp;
-
-    for (int i = 0; i < arr.size(); i++)
-    {
-        int diff = sum - arr[i];
-        if (mpp.find(arr[i]) != mpp.end())
-        {
-            ans.push_back({arr[i], diff});
-            mpp.erase(arr[i]);
-        }
-        else
-            mpp[diff] = i;
-    }
-
-    return ans;
+    cout << "ARRAY: ";
+    for (auto it : arr)
+        cout << it << " ";
+    cout << endl;
 }
 
-void print(vector<pair<int, int>> arr)
-{
-    for (auto i : arr)
-        cout << i.first << " " << i.second << endl;
-}
 int main()
 {
-    vector<int> arr = {9, 7, 2, 4, 1, 3, 2, 2, 5, 7};
-    int size = arr.size();
-    int sum = 10;
-    // vector<pair<int, int>> ans = solve1(arr, size, sum);
-    // vector<pair<int, int>> ans = solve2(arr, size, sum);
-    vector<pair<int, int>> ans = solve3(arr, size, sum);
-    print(ans);
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "BEFORE ";
+    print(arr);
+    sort(arr, 0, n - 1);
+    cout << "AFTER ";
+    print(arr);
     return 0;
 }
