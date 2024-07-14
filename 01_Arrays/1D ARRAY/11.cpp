d55ef025ac9a176e45cbe482ec414f4092fe7849
speedcuberayush <msdakm3334@gmail.com>
2024-07-14 16:56:54 +0530

    session afternoon

--- 01_Arrays/1D ARRAY/11.cpp
+++ 01_Arrays/1D ARRAY/11.cpp
@@ -1,42 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void print(const vector<int> &arr)
+void print(vector<int> &arr)
 {
     for (auto it : arr)
         cout << it << " ";
     cout << endl;
 }
-
-// approach 1
-int solve(vector<int> arr)
+int max1s(vector<int> &arr)
 {
     int ans = 0;
-    int sum = accumulate(arr.begin(), arr.end(), 0);
-    int n = *max_element(arr.begin(), arr.end());
-    return (n * (n + 1) / 2) - sum;
+    int count = 0;
+    for (auto it : arr)
+    {
+        if (it == 1)
+            count++;
+        else
+        {
+            ans = max(ans, count);
+            count = 0;
+        }
+    }
+    return ans;
 }
-// approach 2
-// int solve(vector<int> arr)
-// {
-//     sort(arr.begin(), arr.end());
-//     if (arr[0] != 1)
-//         return 1;
-//     for (int i = 0; i < arr.size(); i++)
-//     {
-//         if (arr[i] + 1 != arr[i + 1])
-//             return arr[i] + 1;
-//     }
-//     return -1;
-// }
-
 int main()
 {
-    vector<int> arr = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> arr = {1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1};
     int n = arr.size();
-    cout << "ORIGINIAL: ";
     print(arr);
-    cout << solve(arr);
-
+    cout << "MAX 1 count: " << max1s(arr);
     return 0;
 }
--- 01_Arrays/1D ARRAY/temp.cpp
+++ 01_Arrays/1D ARRAY/temp.cpp
@@ -6,10 +6,27 @@ void print(vector<int> &arr)
         cout << it << " ";
     cout << endl;
 }
+int max1s(vector<int> &arr)
+{
+    int ans = 0;
+    int count = 0;
+    for (auto it : arr)
+    {
+        if (it == 1)
+            count++;
+        else
+        {
+            ans = max(ans, count);
+            count = 0;
+        }
+    }
+    return ans;
+}
 int main()
 {
     vector<int> arr = {1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1};
     int n = arr.size();
     print(arr);
+    cout << "MAX 1 count: " << max1s(arr);
     return 0;
 }
