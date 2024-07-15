d2bdfe87dd3e3b7e00da8586dc99d5141cb9f2fa
speedcuberayush <msdakm3334@gmail.com>
2024-07-15 11:26:28 +0530

    session morning

--- 01_Arrays/1D ARRAY/temp.cpp
+++ 01_Arrays/1D ARRAY/temp.cpp
@@ -1,44 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void sort(vector<int> &arr, int s, int e)
-{
-    if (s < e)
-    {
-
-        int i = s - 1;
-        for (int j = s; j < e; j++)
-            if (arr[j] < arr[e])
-                swap(arr[++i], arr[j]);
-        swap(arr[++i], arr[e]);
-        sort(arr, s, i - 1);
-        sort(arr, i + 1, e);
-    }
-}
-
-void print(vector<int> arr)
-{
-
-    cout << "ARRAY: ";
-    for (auto it : arr)
-        cout << it << " ";
-    cout << endl;
-}
-
-int main()
-{
-    int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
-
-    vector<int> arr(n);
-    cout << "Enter array: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    cout << "BEFORE ";
-    print(arr);
-    sort(arr, 0, n - 1);
-    cout << "AFTER ";
-    print(arr);
+int main(){
+    
     return 0;
 }
