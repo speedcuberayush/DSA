a792a2c057d90cbfb865dff4db798f6b90417034
speedcuberayush <msdakm3334@gmail.com>
2024-07-15 19:53:08 +0530

    session one timer

--- 01_Arrays/1D ARRAY/temp.cpp
+++ 01_Arrays/1D ARRAY/temp.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 void print(vector<int> arr)
 {
-    for (int it : arr)
+    for (auto it : arr)
         cout << it << " ";
     cout << endl;
 }
-void recbubble(vector<int> &arr, int n)
+
+void sort(vector<int> &arr, int s, int e)
 {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n - i; j++)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+    if (s < e)
+    {
+        int i = s - 1;
+        for (int j = s; j < e; j++)
+            if (arr[j] < arr[e])
+                swap(arr[++i], arr[j]);
+
+        swap(arr[++i], arr[e]);
+        sort(arr, s, i - 1);
+        sort(arr, i + 1, e);
+    }
 }
+
 int main()
 {
-    vector<int> arr = {9, 7, 2, 4, 1, 3, 2, 2, 4, 7};
+    vector<int> arr = {9, 7, 2, 4, 1, 3, 2, 2, 5, 7};
     int n = arr.size();
-    recbubble(arr, n);
+    print(arr);
+    sort(arr, 0, n - 1);
     print(arr);
     return 0;
 }
